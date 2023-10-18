4c3dec3831a2002a6e8aeb4e1627025442a4221e
Patrick Pei <14530807+patrickpei@users.noreply.github.com>
2023-10-18 09:24:55 +0000

    Implement

---
+++ pycccedict/cccedict.py
@@ -0,0 +1,103 @@
+"""CC-CEDICT."""
+
+import gzip
+from pathlib import Path
+from typing import TextIO
+
+class CcCedict:
+    """CC-CEDICT."""
+
+    def __init__(self) -> None:
+        path = Path(__file__).parent / 'data' / 'cedict_1_0_ts_utf-8_mdbg.txt.gz'
+        with gzip.open(path, mode='rt') as file:
+            self._parse_file(file)
+
+    def get_definitions(self, chinese: str) -> list | None:
+        """Gets definitions."""
+        return self._get_field(field='definitions', chinese=chinese)
+
+    def get_pinyin(self, chinese: str) -> list | None:
+        """Gets pinyin."""
+        return self._get_field(field='pinyin', chinese=chinese)
+
+    def get_simplified(self, chinese: str) -> list | None:
+        """Gets simplified."""
+        return self._get_field(field='simplified', chinese=chinese)
+
+    def get_traditional(self, chinese: str) -> list | None:
+        """Gets traditional."""
+        return self._get_field(field='traditional', chinese=chinese)
+
+    def get_entry(self, chinese: str) -> dict | None:
+        """Gets an entry."""
+        # Check simplified.
+        if chinese in self.simplified_to_index:
+            i = self.simplified_to_index[chinese]
+            return self.entries[i]
+
+        # Check traditional.
+        if chinese in self.traditional_to_index:
+            i = self.traditional_to_index[chinese]
+            return self.entries[i]
+
+        return None
+
+    def _get_field(self, field: str, chinese: str) -> str | list | None:
+        """Gets field."""
+        entry = self.get_entry(chinese)
+        if entry is None:
+            return None
+
+        return entry[field]
+
+    def _parse_file(self, file: TextIO) -> None:
+        self.entries = []
+        self.simplified_to_index = {}
+        self.traditional_to_index = {}
+        i = 0
+
+        for line in file:
+            entry = self._parse_line(line)
+            if entry is None:
+                continue
+
+            # Add entry.
+            self.entries.append(entry)
+
+            # Share entries for simplified and traditional.
+            simplified = entry['simplified']
+            traditional = entry['traditional']
+            self.simplified_to_index[simplified] = i
+            self.traditional_to_index[traditional] = i
+            i += 1
+
+    def _parse_line(self, line: str) -> dict | None:
+        # Skip comments.
+        if line.startswith('#'):
+            return None
+
+        # Strip whitespace and trailing slash.
+        line = line.strip()
+        line = line.rstrip('/')
+
+        # Get chinese parts.
+        chinese, english = line.split('/', maxsplit=1)
+        chinese = chinese.strip()
+        traditional_and_simplified, pinyin = chinese.split('[')
+        traditional_and_simplified = traditional_and_simplified.strip()
+        traditional, simplified = traditional_and_simplified.split()
+
+        # Remove brackets around pinyin.
+        pinyin = pinyin[:-1]
+
+        # Get english definitions.
+        senses = english.split('/')
+        glosses = [sense.split(';') for sense in senses]
+        definitions = [definition for gloss in glosses for definition in gloss]
+
+        return {
+            'traditional': traditional,
+            'simplified': simplified,
+            'pinyin': pinyin,
+            'definitions': definitions,
+        }
---
+++ pycccedict/cccedict_test.py
@@ -0,0 +1,40 @@
+"""CC-CEDIT Test."""
+
+import unittest
+
+from pycccedict.cccedict import CcCedict
+
+class CcCedictTest(unittest.TestCase):
+    """CC-CEDIT Test."""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.cccedict = CcCedict()
+
+    def test_get_definitions(self):
+        """Tests get_definitions"""
+        self.assertEqual(self.cccedict.get_definitions('猫')[0], 'cat')
+
+    def test_get_pinyin(self):
+        """Tests get_pinyin"""
+        self.assertEqual(self.cccedict.get_pinyin('猫'), 'mao1')
+
+    def test_get_simplified(self):
+        """Tests get_simplified"""
+        self.assertEqual(self.cccedict.get_simplified('猫'), '猫')
+        self.assertEqual(self.cccedict.get_simplified('貓'), '猫')
+
+    def test_get_traditional(self):
+        """Tests get_traditional"""
+        self.assertEqual(self.cccedict.get_traditional('猫'), '貓')
+        self.assertEqual(self.cccedict.get_traditional('貓'), '貓')
+
+    def test_get_entry(self):
+        """Tests get_entry."""
+        for chinese in ['猫', '貓']:
+            entry = self.cccedict.get_entry(chinese)
+            for field in ['definitions', 'pinyin', 'simplified', 'traditional']:
+                self.assertIn(field, entry)
+
+if __name__ == "__main__":
+    unittest.main()
